c7ccaddceca0e98cf1721f554143f8b941893522
Volodymyr Babych <v.d.babych@gmail.com>
2022-11-14 21:54:16 +0200

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+// Імпортую Вімео та Лодеш
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+//ключ до локал сторедж
+const TIME_KEY = 'currentTime'
+//отримую iframe
+const iframeEl = document.querySelector("iframe");
+//створюю екземпляр 
+const myPlayer = new Player(iframeEl);
+const throttledOnTimeUpdating = throttle(onTimeUpdating, 1000, { 'trailing': false });
 
+
+myPlayer.on('timeupdate', throttledOnTimeUpdating);
+
+myPlayer.on('ended', onVideoEnd);
+
+
+function onTimeUpdating(e) {
+    localStorage.setItem(TIME_KEY, e.seconds)
+}
+
+function onVideoEnd(e) {
+    myPlayer.off('timeupdate', throttledOnTimeUpdating);
+    localStorage.removeItem(TIME_KEY)
+}
+
+
+
+//Ініціалізація початкового значення прокрутки відео
+(() => {
+    const savedTime = localStorage.getItem(TIME_KEY) ?? 0;
+        myPlayer.setCurrentTime(Number(savedTime)).then(function () {})
+        .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log('the time was less than 0 or greater than the video’s duration');
+            break;
+
+        default:
+            console.log('some other error occurred');
+            break;
+    }});
+    
+})();
